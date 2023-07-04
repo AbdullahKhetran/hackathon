a10f9135be8e8f97fd784b811bdbce99c0b6f410
Abdullah Khetran <khetranabdullah@gmail.com>
2023-07-04 19:45:21 +0500

    added schemas

--- sanity/schemas/index.ts
+++ sanity/schemas/index.ts
@@ -1,7 +1,8 @@
 import image from "./image-schema"
 import featuredProduct from "./featured-product"
 import product from "./product"
+import brand from "./brands"
 
-const schemas = [image, featuredProduct, product]
+const schemas = [image, featuredProduct, product, brand]
 
 export default schemas
