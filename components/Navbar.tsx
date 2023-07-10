2d60fe3ba5f87f01444ffaf82d6797e67ce53e28
Abdullah Khetran <khetranabdullah@gmail.com>
2023-07-10 15:35:07 +0500

    updated component

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -8,18 +8,25 @@ import LgNav from './LgNav';
 
 
 export default function Navbar() {
-    const [width, setwidth] = useState(window.innerWidth);
+
+    const [width, setWidth] = useState(0); // cannot use window.innerwidth here because of error, see note below
 
     useEffect(() => {
         const handleWindowResize = () => {
-            setwidth(window.innerWidth);
+            setWidth(window.innerWidth);
         };
 
-        window.addEventListener('resize', handleWindowResize);
+        // Check if window object is available
+        if (typeof window !== 'undefined') {
+            // need to add this check because of vercel error: window exists on borwser (client side) but build is happening on server
 
-        return () => {
-            window.removeEventListener('resize', handleWindowResize);
-        };
+            setWidth(window.innerWidth);
+            window.addEventListener('resize', handleWindowResize);
+
+            return () => {
+                window.removeEventListener('resize', handleWindowResize);
+            };
+        }
     }, []);
 
     // const [navbar, setNavbar] = useState(false)
@@ -32,3 +39,59 @@ export default function Navbar() {
         </div>
     );
 }
+
+
+// function Testing() {
+
+//     if (typeof window !== undefined) {
+//         useEffect(() => {
+
+//             const [width, setwidth] = useState(window.innerWidth);
+
+//             if (typeof window !== undefined) {
+//                 // need to add this check because of vercel error: window exists on borwser (client side) but build is happening on server
+//                 const handleWindowResize = () => {
+//                     setwidth(window.innerWidth);
+//                 };
+//                 window.addEventListener('resize', handleWindowResize);
+
+//                 return () => {
+//                     window.removeEventListener('resize', handleWindowResize);
+//                 };
+//             }
+//         }, []);
+
+//     }
+
+
+
+//     useEffect(() => {
+
+
+//         if (typeof window !== undefined) {
+//             // need to add this check because of vercel error: window exists on borwser (client side) but build is happening on server
+//             const handleWindowResize = () => {
+//                 setwidth(window.innerWidth);
+//             };
+//             window.addEventListener('resize', handleWindowResize);
+
+//             return () => {
+//                 window.removeEventListener('resize', handleWindowResize);
+//             };
+//         }
+//     }, []);
+
+//     // const [navbar, setNavbar] = useState(false)
+//     return (
+//         <div>
+//             {width > 1000 ?
+//                 <LgNav /> :
+//                 <SmNav />
+//             }
+//         </div>
+//     );
+// }
+
+
+// comments
+            // need to add this check because of vercel error: window exists on borwser (client side) but build is happening on server
