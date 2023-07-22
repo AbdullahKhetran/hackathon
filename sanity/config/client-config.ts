5c4ddecbcbeb0d5ecd297e610e7e82058d27bbc3
Abdullah Khetran <khetranabdullah@gmail.com>
2023-07-22 21:27:46 +0500

    fixed error

--- sanity/config/client-config.ts
+++ sanity/config/client-config.ts
@@ -1,6 +1,10 @@
 const clientConfig = {
-    projectId: process.env.SANITY_PROJECT_ID,
-    dataset: process.env.SANITY_DATASET,
+    // projectId: process.env.SANITY_PROJECT_ID,
+    // dataset: process.env.SANITY_DATASET,
+    // can't get from .env file because it is ignored through git
+
+    projectId: "qffp0dty",
+    dataset: "production",
     apiVersion: "2023-07-04",
     useCdn: false,
 }
