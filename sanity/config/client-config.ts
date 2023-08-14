8793730672aed26c48a11594ff39e612a58e1385
Abdullah Khetran <khetranabdullah@gmail.com>
2023-08-14 19:39:04 +0500

    added type for variables

--- sanity/config/client-config.ts
+++ sanity/config/client-config.ts
@@ -1,7 +1,7 @@
 require("dotenv").config
 const clientConfig = {
-    projectId: process.env.SANITY_PROJECT_ID,
-    dataset: process.env.SANITY_DATASET,
+    projectId: process.env.SANITY_PROJECT_ID as string,
+    dataset: process.env.SANITY_DATASET as string,
     apiVersion: "2023-07-04",
     useCdn: false,
 }
