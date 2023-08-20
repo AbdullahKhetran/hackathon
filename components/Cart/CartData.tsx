c3185a417bf29bd54243554a9e9d6b0b374c578a
Abdullah Khetran <khetranabdullah@gmail.com>
2023-08-21 02:32:47 +0500

    restructured code

--- components/Cart/CartData.tsx
+++ components/Cart/CartData.tsx
@@ -16,23 +16,24 @@ export default async function CartPage() {
     const result: Cart[] = await res.json()
     // console.log(result) // an array
 
-    if (result.length === 0) {
+    if (result.length > 0) {
         return (
-            <div>
-                <h1 className="font-bold text-xl">Shopping Cart</h1>
-                <EmptyCart />
+            <div className=" my-18 mx-8 md:mx-16 xl:mx-32 px-4 ">
+                {/* Post request krte hue product id bhi bheje (isko sanity ke product .slug.current ke equal rkhte hain. basically db ki item ki product id sanity ke item ka slug hoga. In dono ko match krte hain) */}
+
+                <h1 className="font-bold text-2xl">Shopping Cart</h1>
+
+                <DisplayProducts res={result} />
+
             </div>
         )
     } else return (
-        <div className=" my-18 mx-8 md:mx-16 xl:mx-32 px-4 ">
-            {/* Post request krte hue product id bhi bheje (isko sanity ke product .slug.current ke equal rkhte hain. basically db ki item ki product id sanity ke item ka slug hoga. In dono ko match krte hain) */}
-
-            <h1 className="font-bold text-2xl">Shopping Cart</h1>
-
-            <DisplayProducts res={result} />
-
+        <div>
+            <h1 className="font-bold text-xl">Shopping Cart</h1>
+            <EmptyCart />
         </div>
     )
+
 }
 
 
