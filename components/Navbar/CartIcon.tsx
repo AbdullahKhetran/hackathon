6fb6269b7b6e3b7dfa08c3d34ec59fb68a616aa8
Abdullah Khetran <khetranabdullah@gmail.com>
2023-08-23 01:20:13 +0500

    added link to cart page

--- components/Navbar/CartIcon.tsx
+++ components/Navbar/CartIcon.tsx
@@ -1,9 +1,14 @@
+"use client"
+import { useAppSelector } from "@/redux/hooks"
 import { ShoppingCart } from "lucide-react"
 import Link from "next/link"
 
 export function LgNavCartIcon() {
+    const userid = useAppSelector((state) => state.auth.value.uid)
+    console.log(userid)
+
     return (
-        <Link href={"/cart"} className="flex flex-col items-end px-1 py-[4px] bg-socialIconbg rounded-[50%]">
+        <Link href={`/cart?userid=${userid}`} className="flex flex-col items-end px-1 py-[4px] bg-socialIconbg rounded-[50%]">
             <span className="bg-[#f02d34] rounded-[50%]  px-1 text-[#eee]  font-semibold  text-xs">0</span>
             {/* make the value dynamic */}
             <ShoppingCart size={20} />
