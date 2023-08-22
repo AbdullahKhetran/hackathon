9272bc64c95651de17c83efe1fc7d1c6e6748ee1
Abdullah Khetran <khetranabdullah@gmail.com>
2023-08-23 02:12:06 +0500

    added redux-persist

--- redux/store.ts
+++ redux/store.ts
@@ -1,13 +1,31 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { configureStore, combineReducers } from "@reduxjs/toolkit";
 import cartReducer from "./features/cartSlice"
 import authReducer from "./features/authSlice"
+import { persistReducer, persistStore } from "redux-persist";
+import storage from "./utils";
+
+const persistConfig = {
+    key: "root",
+    storage
+}
+
+const combinedReducer = combineReducers({
+    cart: cartReducer,
+    auth: authReducer
+})
+
+const persistedReducer = persistReducer(persistConfig, combinedReducer)
 
 export const store = configureStore({
-    reducer: {
-        cart: cartReducer,
-        auth: authReducer
-    }
+    reducer: persistedReducer,
+    middleware: (getDefaultMiddleware) => getDefaultMiddleware({
+        serializableCheck: false
+    }),
+    devTools: true
 })
+
+export const persistor = persistStore(store)
+
 
 // Types
 export type RootState = ReturnType<typeof store.getState>
