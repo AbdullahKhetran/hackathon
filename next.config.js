bcca6cb63d4abd421628da5afb70bac0ec1aa709
Abdullah Khetran <khetranabdullah@gmail.com>
2023-08-23 16:15:21 +0500

    setup cors origin

--- next.config.js
+++ next.config.js
@@ -10,6 +10,25 @@ const nextConfig = {
             },
         ],
     },
+    async headers() {
+        return [
+            {
+                source: "./api/:path*",
+
+                headers: [
+
+                    { key: "Access-Control-Allow-Credentials", value: "true" },
+
+                    { key: "Access-Control-Allow-Origin", value: "https://hackathon-git-development-abdullahkhetran.vercel.app/" },
+
+                    { key: "Access-Control-Allow-Methods", value: "GET,POST" },
+
+                    { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
+
+                ]
+            }
+        ]
+    }
 }
 
 module.exports = nextConfig
