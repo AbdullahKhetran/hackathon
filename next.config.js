55481a4e4cc0eeb4d869cb48248cf49ee5036b58
Abdullah Khetran <khetranabdullah@gmail.com>
2023-08-23 16:23:02 +0500

    removed cors header

--- next.config.js
+++ next.config.js
@@ -1,24 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    async headers() {
-        return [
-            {
-                source: "./api/(.*)",
 
-                headers: [
-
-                    { key: "Access-Control-Allow-Credentials", value: "true" },
-
-                    { key: "Access-Control-Allow-Origin", value: "https://hackathon-git-development-abdullahkhetran.vercel.app/" },
-
-                    { key: "Access-Control-Allow-Methods", value: "GET,POST" },
-
-                    { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
-
-                ]
-            }
-        ]
-    },
     images: {
         remotePatterns: [
             {
