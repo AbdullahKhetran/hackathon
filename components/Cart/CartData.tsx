dc078f1b7e88d21b789954a8439b499498b4e958
Abdullah Khetran <khetranabdullah@gmail.com>
2023-08-23 16:28:10 +0500

    testing with dummy data

--- components/Cart/CartData.tsx
+++ components/Cart/CartData.tsx
@@ -17,15 +17,14 @@ async function getData(uid: string) {
 export default async function CartPage() {
 
     const uid = useAppSelector((state) => state.auth.uid)
-    getData(uid)
 
-    const result = await getData(uid)
-    console.log(result)
-    console.log("Result array length", result.length)
+    // const result = await getData(uid)
+    // console.log(result)
+    // console.log("Result array length", result.length)
 
 
     // Dummy Data
-    // const result: Cart[] = [{ "id": 23, "userid": "251a8eca-a6af-49d9-b839-515f90e0048b", "productid": "02e5b664-fe39-4d4a-a712-5c3345a39a3f", "quantity": 3 }, { "id": 24, "userid": "251a8eca-a6af-49d9-b839-515f90e0048b", "productid": "5a4cfa64-c039-49b5-86e8-e9f74979c563", "quantity": 1 }]
+    const result: Cart[] = [{ "id": 23, "userid": "251a8eca-a6af-49d9-b839-515f90e0048b", "productid": "02e5b664-fe39-4d4a-a712-5c3345a39a3f", "quantity": 3 }, { "id": 24, "userid": "251a8eca-a6af-49d9-b839-515f90e0048b", "productid": "5a4cfa64-c039-49b5-86e8-e9f74979c563", "quantity": 1 }]
 
 
     // -------------------------------
