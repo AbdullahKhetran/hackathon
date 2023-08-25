711151965343c6819a453d0d017073070a54f4c9
Abdullah Khetran <khetranabdullah@gmail.com>
2023-08-25 05:56:11 +0500

    combined types in a single file

--- types/Brands.ts
+++
@@ -1,7 +0,0 @@
-export type Brand = {
-    _id: string,
-    _createdAt: Date,
-    name: string,
-    logo: string,
-    slug: string,
-}
---
+++ types/sanity.ts
@@ -0,0 +1,49 @@
+import { Image, PortableTextBlock } from "sanity";
+
+
+export type Brand = {
+    _id: string,
+    _createdAt: Date,
+    name: string,
+    logo: string,
+    slug: string,
+}
+
+
+export type Product = {
+    _id: string,
+    _createdAt: Date,
+    name: string,
+    gender: string,
+    category: string,
+    // image: string,
+    image: Image,
+    slug: {
+        current: string,
+        _type: string
+    },
+    price: number,
+    details: PortableTextBlock[],
+    care: PortableTextBlock[],
+
+}
+
+// export type FeaturedProduct = {
+//     _id: string,
+//     createdAt: Date,
+//     image: string,
+//     title: string,
+//     price: number,
+//     slug: {
+//         current: string,
+//         _type: string
+//     },
+// }
+
+// export type Image = {
+//     Id: string,
+//     _createdAt: Date,
+//     title: string,
+//     image: string,
+//     slug: string,
+// }
