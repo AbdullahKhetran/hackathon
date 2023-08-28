f9daa37768487f6c7626222246c0953df87c6a21
Abdullah Khetran <khetranabdullah@gmail.com>
2023-08-28 12:52:58 +0500

    commented outimage

--- app/api/stripe-checkout/route.ts
+++ app/api/stripe-checkout/route.ts
@@ -129,7 +129,7 @@ export async function POST(request: NextRequest) {
                             currency: "usd",
                             product_data: {
                                 name: item.name,
-                                images: [item.image],
+                                // images: [item.image],
                             },
                             unit_amount: item.price * 100,
                         },
