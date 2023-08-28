7e2cc0ecbfc1dd3cface553cf9e1cca0f4e0f554
Abdullah Khetran <khetranabdullah@gmail.com>
2023-08-28 07:35:44 +0500

    added log statement for key

--- lib/stripe.ts
+++ lib/stripe.ts
@@ -4,11 +4,9 @@ let stripePromise: Promise<Stripe | null>
 
 export const getStripePromise = () => {
     const key = process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY;
-
+    console.log("key type is ", typeof key)
     if (!stripePromise && key) {
         stripePromise = loadStripe(key);
-        return stripePromise;
-    } else {
-        console.log("Promise or key is undefined or null")
     }
+    return stripePromise;
 };
